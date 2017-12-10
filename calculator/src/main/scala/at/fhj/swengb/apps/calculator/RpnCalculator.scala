9b1df1ef38596fea52b2f7e6c0d399cf672a40e2
mhutter07 <martin.hutter@edu.fh-joanneum.at>
2017-12-10 10:27:38 +0100

    all tests done - timesheetSpec done - some minor improvements on css and fxml

--- calculator/src/main/scala/at/fhj/swengb/apps/calculator/RpnCalculator.scala
+++ calculator/src/main/scala/at/fhj/swengb/apps/calculator/RpnCalculator.scala
@@ -14,14 +14,19 @@ object RpnCalculator {
     * @param s a string representing a calculation, for example '1 2 +'
     * @return
     */
-  def apply(s: String): Try[RpnCalculator] = ???
-
+  def apply(s : String): Try[RpnCalculator] = {
+    if (s.isEmpty) {Try(RpnCalculator())}
+    else try {
+      s.split("\\s").map(x => Op(x)).toList.foldLeft(Try(RpnCalculator()))((acc, op) => acc.get.push(op))
+    } catch {
+      case e : Exception => Try[RpnCalculator] (throw e)
+    }
+  }
 }
-
 /**
   * Reverse Polish Notation Calculator.
   *
-  * @param stack a datastructure holding all operations
+  * @param stack a data structure holding all operations
   */
 case class RpnCalculator(stack: List[Op] = Nil) {
 
@@ -32,7 +37,21 @@ case class RpnCalculator(stack: List[Op] = Nil) {
     * @param op
     * @return
     */
-  def push(op: Op): Try[RpnCalculator] = ???
+  def push(op : Op): Try[RpnCalculator] = op match {
+    case value : Val => Try(RpnCalculator(stack :+ value))
+    case binop : BinOp => try {
+      def examineStack(x : RpnCalculator) : Val = x.peek() match {
+          case value: Val => value
+          case _ => throw new NoSuchElementException
+        }
+      val nextStep = pop()._2
+      val putOnStack : Val = binop.eval(examineStack(this), examineStack(nextStep))
+      nextStep.pop()._2.push(putOnStack)
+    }
+      catch {
+        case e : Exception => Try[RpnCalculator](throw e)
+      }
+    }
 
   /**
     * Pushes val's on the stack.
@@ -42,26 +61,36 @@ case class RpnCalculator(stack: List[Op] = Nil) {
     * @param op
     * @return
     */
-  def push(op: Seq[Op]): Try[RpnCalculator] = ???
+  def push(op: Seq[Op]): Try[RpnCalculator] = {
+    op.foldLeft(Try(RpnCalculator()))((acc, op) => acc.get.push(op))
+  }
 
   /**
     * Returns an tuple of Op and a RevPolCal instance with the remainder of the stack.
     *
     * @return
     */
-  def pop(): (Op, RpnCalculator) = ???
+  def pop(): (Op, RpnCalculator) = {
+    (stack.head, RpnCalculator(stack.tail))
+  }
+
 
   /**
     * If stack is nonempty, returns the top of the stack. If it is empty, this function throws a NoSuchElementException.
     *
     * @return
     */
-  def peek(): Op = ???
+  def peek(): Op = {
+    if (stack.nonEmpty) {stack.head}
+    else {throw new NoSuchElementException}
+  }
 
   /**
     * returns the size of the stack.
     *
     * @return
     */
-  def size: Int = ???
+  def size: Int = {
+    stack.size
+  }
 }
